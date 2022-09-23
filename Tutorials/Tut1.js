46f5f9750270f01b86f6532ee4e41b8fcfa1e9f1
Jayant Baid <79745190+jayant-baid@users.noreply.github.com>
2022-09-23 10:44:09 +0530

    Functions and DOM

---
+++ Tutorials/Tut1.js
@@ -0,0 +1,46 @@
+// FUNCTIONS
+
+function countDown() {
+    // consol.log() is like cout
+    console.log(5)
+    console.log(4)
+    console.log(3)
+    console.log(2)
+    console.log(1)
+}
+
+// Setting up the the race 🏎 🏎 🏎
+
+countDown()
+// GO! 🏁
+// Players are running the race 🏎 💨
+// Race is finished! 🍾
+
+// Get ready for a new race 🏎 🏎 🏎
+countDown()
+
+// EXCERCISE
+// let is like auto datatype
+let lap1 = 34
+let lap2 = 33
+let lap3 = 36
+
+// Create a function that logs out the sum of all the lap times
+
+function sum() {
+    console.log(lap1 + lap2 + lap3)
+}
+
+sum()
+
+// FOR LOOP
+for (let i = 0; i < 3; i++) {
+
+}
+
+/* DOM (DOCUMENT OBJECT MODEL)
+ aka how you use JAVASCRIPT to modify a website
+ DOCUMENT: HTML Document
+ OBJECT: document in JS is of the datatype object meaning they've taken the HTML DOC and shoved it into JS object.
+ MODEL: a representation or modelation
+ */
