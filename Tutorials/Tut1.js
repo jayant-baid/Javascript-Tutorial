877280e65657e38d2307fae7a25ce9a6463716a4
Jayant Baid <79745190+jayant-baid@users.noreply.github.com>
2022-09-23 12:29:04 +0530

    Added some basic concepts & strings

--- Tutorials/Tut1.js
+++ Tutorials/Tut1.js
@@ -1,3 +1,33 @@
+
+// let firstbatch = 5
+// let secondbatch = 7
+// let count = firstbatch + secondbatch
+// console.log(count)
+
+// EXCERCISE
+// 1. Create two variables, myAge and humanDogRatio
+// 2. Multiply the two together and store the result in myDogAge
+// 3. Log myDogAge to the console
+/*
+let myAge = 19
+let humanDogRatio = 1 / 7
+let myDogAge = myAge * humanDogRatio
+console.log(myDogAge)
+*/
+
+// EXCERCISE
+// Create a variable bonusPoints. Initialize it as 50. Increase it to 100.
+// Decrease it down to 25, and then finally increase it to 70
+/*
+let bonusPoints = 50
+bonusPoints += 100
+console.log(bonusPoints)
+bonusPoints -= 25
+console.log(bonusPoints)
+bonusPoints += 70
+console.log(bonusPoints)
+*/
+
 // FUNCTIONS
 
 function countDown() {
@@ -44,3 +74,28 @@ for (let i = 0; i < 3; i++) {
  OBJECT: document in JS is of the datatype object meaning they've taken the HTML DOC and shoved it into JS object.
  MODEL: a representation or modelation
  */
+
+// STRINGS
+// we can use single quote also
+let username = "Jayant"
+console.log(username)
+
+// CONCATENATION
+let message = "You have new notifications"
+console.log(username + ", " + message + "!")
+
+// STRINGS VS NUMBERS
+let number = 42
+let greeting = "Hi, my name is "
+let myGreeting = greeting + number
+console.log(myGreeting)
+// OUTPUT: Hi, my name is 42
+/* 42 has been turned into string
+   In the battle b/w string & number, string wins */
+
+let points = 4
+let bonusPoints = "10"
+let totalPoints = points + bonusPoints
+// points gets typecast from int to strings 
+console.log(totalPoints)
+//    OUTPUT: 410
